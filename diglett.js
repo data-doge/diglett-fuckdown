e68175a785de0ef65b5855b6529cea49f63a7a56
euglazer <eugenestapler@gmail.com>
2015-03-29 14:54:35 +1300

    shit cunts come and go

--- diglett.js
+++ diglett.js
@@ -2,12 +2,12 @@ var diglett = 'http://fc06.deviantart.net/fs70/f/2010/165/9/4/050___Diglett_by_c
 
 $(document).ready(function() {
   $('img').attr('src',diglett);
-  $('.title').text('diglett');
+  $('.title').text('shit cunts come and go, aye');
   $('.score').text('420');
   $('.comments').text('420 digletts yall');
   $('.author').text('diglett');
   $('.subreddit').text('/r/diglett');
   $('time').replaceWith('at 4:20');
-  $('.option').text('piss on the floor like a shit-cunt');
+  $('.option').text('piss on the floor like a shit cunt');
   $('.choice').text('diglett');
 });
