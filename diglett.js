205c2492aecbe9fd3358ba5dbe746cfb24643df4
euglazer <eugenestapler@gmail.com>
2015-03-29 14:37:15 +1300

    diglett all up in everywhere

--- diglett.js
+++ diglett.js
@@ -1,18 +1,13 @@
 var diglett = 'http://fc06.deviantart.net/fs70/f/2010/165/9/4/050___Diglett_by_cammarin.png';
-var strikes = 0;
 
-$('body').on('click', function(e) {
-  e.preventDefault();
-  strikes++;
-  if (strikes < 3) {
-    alert('STOP REDDITING');
-  } else {
-    var begging = prompt('YOU ARE AN UNPRODUCTIVE TAINT -- SAY SORRY');
-    if (begging.toLowerCase() !== 'sorry') {
-      $('body').html('<h1>YOU\'VE BEEN SERVED</h1><img src=' + diglett + '>');
-      $('h1').css({fontSize: '100px', color: 'white'});
-    } else {
-      alert('you are forgiven');
-    }
-  }
+$(document).ready(function() {
+  $('img').attr('src',diglett);
+  $('.title').text('diglett');
+  $('.score').text('420');
+  $('.comments').text('420 digletts yall');
+  $('.author').text('diglett');
+  $('.subreddit').text('/r/diglett');
+  $('time').replaceWith('at 4:20');
+  $('.option').text('piss on the floor like a shit-cunt');
+  $('.choice').text('diglett');
 });
